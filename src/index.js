302bcaf4e397eb57bdd8c786922fb74b93e8a246
Martin Sasák <martin.sasak@sequel.sk>
2018-07-02 22:41:53 +0200

    Added Redux to the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import "tachyons";
+import { searchRobots, requestRobots } from './reducers';
+import 'tachyons';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-    <div>
+    <Provider store={store}>
     	<App />
-    </div>
+    </Provider>
 	,
 	document.getElementById('root'));
 registerServiceWorker();
